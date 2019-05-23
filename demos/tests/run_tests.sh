f1e6ffbfaccde36ca1a18219af2b4d3d050e934f
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-05-22 22:36:31 -0700

    find and run all tests in test file

--- demos/tests/run_tests.sh
+++ demos/tests/run_tests.sh
@@ -2,15 +2,27 @@
 
 source ../settings.sh
 
-test_file_name=$1
-test_names='[test, test2, test3]'
+test_file_base=$1
+
+test_name_pattern='^[[:space:]]*test[_a-zA-Z0-9]*()'
+
+readarray test_names_array < <(grep $test_name_pattern ${test_file_base}.P | cut -d "(" -f1 )
+test_names="[ "
+delimiter=""
+for tn in "${test_names_array[@]}"
+do
+    test_names+=$delimiter
+    test_names+=$tn
+    delimiter=", "
+done
+test_names+="]"
 
 xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
 
 set_prolog_flag(unknown, fail).
 
 ['../rules/array_views'].
-[${test_file_name}].
+[${test_file_base}].
 [basics].
 [user].
 
@@ -28,7 +40,7 @@ do_tests(TestFileName, TestNames) :-
 
 end_of_file.
 
-do_tests(${test_file_name}, ${test_names}).
+do_tests(${test_file_base}, ${test_names}).
 
 
 END_XSB_STDIN
