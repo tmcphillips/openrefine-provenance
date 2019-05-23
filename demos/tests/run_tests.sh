499f4e8a28581300da3f45ee7d9fd5a53208ec9a
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-05-22 19:11:33 -0700

    leave only facts and rules in test file

--- demos/tests/run_tests.sh
+++ demos/tests/run_tests.sh
@@ -2,4 +2,15 @@
 
 source ../settings.sh
 
-xsb --quietload --noprompt --nofeedback --nobanner < test_array_views.P
+test_file_name=$1
+test_name='test'
+
+xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
+
+set_prolog_flag(unknown, fail).
+
+[${test_file_name}].
+['../rules/array_views'].
+${test_name}().
+
+END_XSB_STDIN
