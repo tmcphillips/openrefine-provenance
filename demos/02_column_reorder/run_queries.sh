6f637be0cecca6ab6c4ef2b5d8b886c8859d0b63
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-05-27 23:01:41 -0700

    started demo with column reorderings

---
+++ demos/02_column_reorder/run_queries.sh
@@ -0,0 +1,101 @@
+#!/usr/bin/env bash
+#
+# ./run_queries.sh &> run_queries.txt
+
+source ../settings.sh
+
+xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
+
+['$RULES_DIR/general_rules'].
+['$RULES_DIR/query_rules'].
+['cleaning_history'].
+
+%set_prolog_flag(unknown, fail).
+
+%-------------------------------------------------------------------------------
+banner( 'Q1',
+        'What are the names of files from which data was imported?',
+        'q1(SourceUri)').
+[user].
+
+:- table q1/1.
+q1(SourceUri) :-
+
+    source(_, SourceUri, _).
+
+end_of_file.
+printall(q1(_)).
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+banner( 'Q2',
+        'What are the original names of each column in this data set?',
+        'q2(ColumnName)').
+[user].
+
+:- table q2/1.
+q2(ColumnName) :-
+
+    import_state('biblio.csv', _, _, ImportStateId),
+    column_name(_, ImportStateId, ColumnName).
+
+end_of_file.
+
+printall(q2(_)).
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+banner( 'Q3',
+        'What new names are assigned to these columns?',
+        'q3(ColumnName, NewColumnName)').
+[user].
+
+:- table q3/2.
+q3(ColumnName, NewColumnName) :-
+
+    import_state('biblio.csv', DatasetId, _, _),
+    column_rename(DatasetId, _, _, ColumnName, NewColumnName).
+
+end_of_file.
+
+printall(q3(_,_)).
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+banner( 'Q4',
+        'How many steps are there in the data cleaning workflow including data import?',
+        'q4(StateCount)').
+[user].
+
+:- table q4/1.
+q4(StateCount) :-
+
+    import_state('biblio.csv', DatasetId, _, _),
+    array(ArrayId, DatasetId),
+    count(state(_, ArrayId, _), StateCount).
+
+end_of_file.
+
+printall(q4(_)).
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+banner( 'Q5',
+        'What are the final names of each column in this data set?',
+        'q5(ColumnName)').
+[user].
+
+:- table q5/1.
+q5(ColumnName) :-
+
+    import_state('biblio.csv', _, ImportArrayId, _),
+    final_array_state(ImportArrayId, FinalArrayStateId),
+    column_name(_, FinalArrayStateId, ColumnName).
+
+end_of_file.
+
+printall(q5(_)).
+%-------------------------------------------------------------------------------
+
+END_XSB_STDIN
+
---
+++ demos/02_column_reorder/run_reports.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+#
+# ./run_reports.sh &> run_reports.txt
+
+source ../settings.sh
+
+xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
+
+['$RULES_DIR/general_rules'].
+['$RULES_DIR/query_rules'].
+['$RULES_DIR/report_rules'].
+['cleaning_history'].
+
+%set_prolog_flag(unknown, fail).
+
+%-------------------------------------------------------------------------------
+banner( 'Report 1',
+        'Print the names of the columns in order at the initially imported state',
+        'r1()').
+[user].
+r1() :-
+
+    import_state('biblio.csv', _, _, ImportStateId),
+    print_array_header(ImportStateId).
+
+end_of_file.
+r1().
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+banner( 'Report 2',
+        'Print the names of the columns in order at the initially imported state',
+        'r2()').
+[user].
+r2() :-
+
+    import_state('biblio.csv', _, ArrayId, _),
+    final_array_state(ArrayId, FinalStateId),
+    print_array_header(FinalStateId).
+
+end_of_file.
+r2().
+%-------------------------------------------------------------------------------
+
+
+%-------------------------------------------------------------------------------
+banner( 'Report 3',
+        'Print the names of the columns in the order at any intermediate states',
+        'r3()').
+[user].
+r3() :-
+
+    import_state('biblio.csv', _, ArrayId, ImportStateId),
+    final_array_state(ArrayId, FinalStateId),
+    state(StateId, ArrayId, _),
+    StateId \== ImportStateId,
+    StateId \== FinalStateId,
+    print_array_header(StateId),
+    fail
+    ;
+    true.
+
+end_of_file.
+r3().
+%-------------------------------------------------------------------------------
+
+
+END_XSB_STDIN
+
