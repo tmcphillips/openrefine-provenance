bfb0558e74a6709ee6ee7da525f63ab2636031c8
Timothy McPhillips <tmcphillips@absoluteflow.com>
2019-06-02 11:20:08 -0700

    report of column names in order now works

--- demos/03_poster_demo/run_all.sh
+++ demos/03_poster_demo/run_all.sh
@@ -1,2 +1,2 @@
 ./run_queries.sh &> run_queries.txt
-#./run_reports.sh &> run_reports.txt
+./run_reports.sh &> run_reports.txt
---
+++ demos/03_poster_demo/run_reports.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# ./run_reports.sh &> run_reports.txt
+
+source ../settings.sh
+
+xsb --quietload --noprompt --nofeedback --nobanner << END_XSB_STDIN
+
+['$RULES_DIR/general_rules'].
+['$RULES_DIR/query_rules'].
+['$RULES_DIR/report_rules'].
+['cleaning_history'].
+
+%set_prolog_flag(unknown, fail).
+
+%-------------------------------------------------------------------------------
+banner( 'Report 1',
+        'Print the names of the columns in order at the initially imported state',
+        'r1()').
+[user].
+r1() :-
+
+    import_state('biblio.csv', _, _, ImportStateId),
+    array_header(ImportStateId, Header),
+    writeln(Header).
+
+end_of_file.
+r1().
+%-------------------------------------------------------------------------------
+
+%-------------------------------------------------------------------------------
+banner( 'Report 2',
+        'Print the names of the columns in order at the final state',
+        'r2()').
+[user].
+r2() :-
+
+    import_state('biblio.csv', _, ArrayId, _),
+    final_array_state(ArrayId, FinalStateId),
+    array_header(FinalStateId, Header),
+    writeln(Header).
+
+end_of_file.
+r2().
+%-------------------------------------------------------------------------------
+
+
+END_XSB_STDIN
+
